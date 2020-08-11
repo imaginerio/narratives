05b3a608bcefaf7ced1edf4c7badd251309da4d0
David Heyman <dave@axismaps.com>
2020-08-11 22:44:44 +0100

    Proof of concept reading database

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
-}
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import Projects from '../components/Projects';
 
-export default HomePage;
+const Home = () => {
+  const client = new ApolloClient({
+    uri: 'http://localhost:3000/admin/api',
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h1>NextJS GraphQL Apollo App</h1>
+        <Projects />
+      </div>
+    </ApolloProvider>
+  );
+};
+
+export default Home;
