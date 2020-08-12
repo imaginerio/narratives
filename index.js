5a95b775d6fb685303b0d07e00967730f27ecb8a
David Heyman <dave@axismaps.com>
2020-08-12 13:07:35 +0100

    Fixed merge issues

--- index.js
+++ index.js
@@ -24,53 +24,6 @@ keystone.createList('User', UserSchema);
 keystone.createList('Project', ProjectSchema);
 keystone.createList('Tag', TagSchema);
 
-  // Instead of a boolean, you can return a GraphQL query:
-  // https://www.keystonejs.com/api/access-control#graphqlwhere
-  return { id: user.id };
-};
-
-const userIsAdminOrOwner = auth => {
-  const isAdmin = access.userIsAdmin(auth);
-  const isOwner = access.userOwnsItem(auth);
-  return isAdmin || isOwner;
-};
-
-const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
-
-keystone.createList('User', {
-  fields: {
-    name: { type: Text },
-    email: {
-      type: Text,
-      isUnique: true,
-    },
-    isAdmin: {
-      type: Checkbox,
-      // Field-level access controls
-      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
-      access: {
-        update: access.userIsAdmin,
-      },
-    },
-    password: {
-      type: Password,
-    },
-    projects: {
-      type: Relationship,
-      ref: 'Project.user',
-      many: true,
-    },
-  },
-  // List-level access controls
-  access: {
-    read: access.userIsAdminOrOwner,
-    update: access.userIsAdminOrOwner,
-    create: access.userIsAdmin,
-    delete: access.userIsAdmin,
-    auth: true,
-  },
-});
-
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
   list: 'User',
@@ -81,13 +34,13 @@ keystone.createList('Project', ProjectSchema);
 module.exports = {
   keystone,
   apps: [
-    new SessionAuth(keystone),
     new GraphQLApp(),
     new AdminUIApp({
       name: PROJECT_NAME,
       enableDefaultRoute: false,
       authStrategy,
     }),
+    new SessionAuth(keystone),
     new NextApp({ dir: 'src' }),
   ],
 };
