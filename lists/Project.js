e087022bc63d3a5e35919b372fbae20c70caa46f
David Heyman <dave@axismaps.com>
2020-09-07 18:43:04 +0100

    Added authentication

--- lists/Project.js
+++ lists/Project.js
@@ -1,7 +1,25 @@
 const { Text, Relationship, Select } = require('@keystonejs/fields');
 const { Wysiwyg } = require('@keystonejs/fields-wysiwyg-tinymce');
 
+const defaultAuth = ({ authentication: { item } }) => {
+  if (item) {
+    return {
+      user: {
+        id: item.id,
+      },
+    };
+  }
+  return false;
+};
+
 module.exports = {
+  access: {
+    auth: true,
+    create: ({ authentication: { item } }) => item !== undefined,
+    read: defaultAuth,
+    update: defaultAuth,
+    delete: defaultAuth,
+  },
   fields: {
     title: {
       type: Text,
--- lists/Tag.js
+++ lists/Tag.js
@@ -1,6 +1,15 @@
 const { Text } = require('@keystonejs/fields');
 
+const defaultAuth = ({ authentication: { item } }) => item !== undefined;
+
 module.exports = {
+  access: {
+    auth: true,
+    create: defaultAuth,
+    read: defaultAuth,
+    update: defaultAuth,
+    delete: defaultAuth,
+  },
   fields: {
     name: {
       type: Text,
