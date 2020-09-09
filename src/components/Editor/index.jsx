fba2e70f7e746721bb437d9d78af471dd70c053b
David Heyman <dave@axismaps.com>
2020-09-10 00:20:11 +0100

    Update title and description

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -19,10 +19,30 @@ const GET_SLIDES = gql`
   }
 `;
 
+const UPDATE_SLIDE_TITLE = gql`
+  mutation UpdateSlideTitle($slide: ID!, $value: String) {
+    updateSlide(id: $slide, data: { title: $value }) {
+      id
+      title
+    }
+  }
+`;
+
+const UPDATE_SLIDE_DESCRIPTION = gql`
+  mutation UpdateSlideTitle($slide: ID!, $value: String) {
+    updateSlide(id: $slide, data: { description: $value }) {
+      id
+      description
+    }
+  }
+`;
+
 const Editor = ({ slide }) => {
   const { loading, error, data } = useQuery(GET_SLIDES, {
     variables: { slide },
   });
+  const [updateTitle] = useMutation(UPDATE_SLIDE_TITLE);
+  const [updateDescription] = useMutation(UPDATE_SLIDE_DESCRIPTION);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -34,11 +54,31 @@ const Editor = ({ slide }) => {
           <Form>
             <Form.Field>
               <label>Title</label>
-              <Input value={data.Slide.title || ''} />
+              <Input
+                value={data.Slide.title || ''}
+                onChange={(e, { value }) =>
+                  updateTitle({
+                    variables: {
+                      slide,
+                      value,
+                    },
+                  })
+                }
+              />
             </Form.Field>
             <Form.Field>
               <label>Description</label>
-              <Form.TextArea />
+              <Form.TextArea
+                value={data.Slide.description || ''}
+                onChange={(e, { value }) =>
+                  updateDescription({
+                    variables: {
+                      slide,
+                      value,
+                    },
+                  })
+                }
+              />
             </Form.Field>
           </Form>
         </Grid.Column>
