01804dfbe3e546bf8fa262b133e4db42a6ce3a3d
David Heyman <dave@axismaps.com>
2020-09-11 11:45:11 +0100

    Full size map

--- src/components/Atlas/index.jsx
+++ src/components/Atlas/index.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactMapGL from 'react-map-gl';
 
-const Atlas = ({ handler, viewport }) => (
-  <ReactMapGL
-    mapboxApiAccessToken="pk.eyJ1IjoiYXhpc21hcHMiLCJhIjoieUlmVFRmRSJ9.CpIxovz1TUWe_ecNLFuHNg"
-    mapStyle="/style.json"
-    width={400}
-    height={400}
-    {...viewport}
-    onViewportChange={nextViewport => handler(nextViewport)}
-  />
-);
+const Atlas = ({ handler, viewport }) => {
+  return (
+    <ReactMapGL
+      mapboxApiAccessToken="pk.eyJ1IjoiYXhpc21hcHMiLCJhIjoieUlmVFRmRSJ9.CpIxovz1TUWe_ecNLFuHNg"
+      mapStyle="/style.json"
+      width="100%"
+      height="100%"
+      {...viewport}
+      onViewportChange={nextViewport => handler(nextViewport)}
+    />
+  );
+};
 
 Atlas.propTypes = {
   handler: PropTypes.func.isRequired,
